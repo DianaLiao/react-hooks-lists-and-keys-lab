10f4407bd50b32ee6de51757b3ecabe6d3d5e2dd
Diana Liao <dijali@gmail.com>
2021-04-26 22:31:46 -0400

    commit tree cc021aec9a13586e441b61f5515c80c969c761a4

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,15 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map(projObj => {
+        return <ProjectItem 
+        key={projObj.id}
+        name={projObj.name}
+        about={projObj.about}
+        technologies={projObj.technologies}
+        />})}
+      </div>
     </div>
   );
 }
